e5f3a8d4bbccaadacb23f19fe6f29fc1a8c907e3
AshithGannarapu16 <ashithgannarapu.com>
2024-02-23 13:51:52 +0530

    exports

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import OrderedList from "./components/list/orderedlist/orderedlist";
 import UnorderedList from "./components/list/unorderedlist/unorderedlist"
 import Table from "./components/table/table";
 import Counter from "./components/counter/counter";
+import Ashith,{HeadingComponent2 as Ashithh,HeadingComponent3 as Gannarapu} from "./components/heading/heading";
+import { Greeting } from "./components/greeting/greeting";
+import { TodoList } from "./components/todo/todo-list";
+import Buttoncomponent from "./components/classBased/button-component";
+import Box from "./components/box/class-box";
+// import Box from "./components/box/box";
+import ApiCall from "./components/exceptionalHandling/api-call";
+
 
 function App(){
   return(
     
     <div>
-    <h3>app.js</h3>
-    {/* <ButtonComponent/>
-    <ListComponent/>
-    <OrderedList/>
-    <UnorderedList/>
-    <Table/> */}
-    <Counter/>
+
+     <ApiCall/> 
+    
+
 
 
 
   
-    <h3>end of buttoncomponent</h3>
+    
     </div>
   )
 }
